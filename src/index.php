5f0222b3fb91f3397cd2ab86408f2521aae99bd3
Alex Galinier <nawax88@gmail.com>
2013-09-19 13:53:40 +0100

    Init web site structure

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<link rel="stylesheet/less" type="text/css" href="less/styles.less" />
+		<script type="text/javascript">
+		    less = {
+		        env: "development"
+		    };
+		</script>
+		<script src="js/less.js" type="text/javascript"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
